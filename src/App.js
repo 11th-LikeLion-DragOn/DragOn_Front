58dcdcc103ac1f494fa4b724649d477d55683947
y0ung22 <amy6484@ewhain.net>
2023-11-17 01:02:23 +0900

    feat: App.js 라우팅 정리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//pages
 import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <>
-      <LoginPage />
+      <Router>
+        <Routes>
+          {/*로그인/회원가입*/}
+          <Route path={"/login"} element={<LoginPage />}></Route>
+          <Route path={"/signup"} element={<SignupPage />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
