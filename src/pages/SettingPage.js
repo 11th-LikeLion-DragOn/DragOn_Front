c8c7875e7da0331e9a42ff0df12a5e09a31b15f6
binys21 <binys1121@ewhain.net>
2023-11-17 22:26:37 +0900

    feat: 설정페이지 ui 수정

--- src/pages/SettingPage.js
+++ src/pages/SettingPage.js
@@ -14,40 +14,45 @@ const SettingPage = () => {
       <Wrapper>
         <Info>
           <img src={profile1} />
-          <div className="name">농담곰</div>
-          <div className="nickname">damgom333</div>
+          <div className="info-text">
+            <div className="name">농담곰</div>
+            <div className="nickname">damgom333</div>
+          </div>
         </Info>
 
         <Excharge>
           보유한 여의주 1개
-          <div className="chargebtn">
-            <img src={marblePurple} />
-            충전하기
-          </div>
+          <img src={marblePurple} />
+          <div className="chargebtn">충전하기</div>
         </Excharge>
         <Profile>
-          <span>프로필</span>
-          <div>
-            닉네임 변경 <img src={clickRight} />
+          <div className="text">프로필</div>
+          <div className="profile-item">
+            <div>닉네임 변경</div>
+            <img src={clickRight} />
           </div>
           <hr></hr>
-          <div>
-            챌린지 성향 테스트 <img src={clickRight} />
+          <div className="profile-item">
+            <div>챌린지 성향 테스트</div>
+            <img src={clickRight} />
           </div>
           <hr></hr>
         </Profile>
         <Account>
-          <span>계정</span>
-          <div>
-            비밀번호 변경 <img src={clickRight} />
+          <div className="text">계정</div>
+          <div className="account-item">
+            <div>비밀번호 변경</div>
+            <img src={clickRight} />
           </div>
           <hr></hr>
-          <div>
-            로그아웃 <img src={clickRight} />
+          <div className="account-item">
+            <div>로그아웃</div>
+            <img src={clickRight} />
           </div>
           <hr></hr>
-          <div>
-            계정 탈퇴 <img src={clickRight} />
+          <div className="account-item">
+            <div>계정 탈퇴</div>
+            <img src={clickRight} />
           </div>
           <hr></hr>
         </Account>
@@ -61,11 +66,18 @@ const Wrapper = styled.div`
   padding: 26px 25px 0px 28px;
 `;
 const Info = styled.div`
+  margin-top: 12px;
   display: flex;
   img {
     width: 57px;
     height: 57px;
     flex-shrink: 0;
+  }
+  .info-text {
+    margin-top: 11px;
+    margin-left: 16px;
+    display: flex;
+    flex-direction: column;
   }
   .name {
     font-size: 16px;
@@ -74,6 +86,9 @@ const Info = styled.div`
     line-height: normal;
   }
   .nickname {
+    margin-top: 2px;
+    display: flex;
+    flex-direction: column;
     color: var(--gray_02, #b5b5b5);
     font-feature-settings: "clig" off, "liga" off;
     font-family: Pretendard;
@@ -84,18 +99,26 @@ const Info = styled.div`
   }
 `;
 const Excharge = styled.div`
+  color: var(--gray_02, #b5b5b5);
+  margin-top: 32px;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  padding-left: 16px;
+  align-items: center;
   display: flex;
   width: 340px;
   height: 47px;
   border-radius: 6px;
   background: var(--purple_02, #f5f1ff);
   img {
-    margin-left: 144px;
+    margin-left: 164px;
   }
   .chargebtn {
+    margin-left: 3px;
+    margin-top: 4px;
     color: var(--purple_01, #8438ff);
-    font-feature-settings: "clig" off, "liga" off;
-    font-family: Pretendard;
     font-size: 12px;
     font-style: normal;
     font-weight: 400;
@@ -103,6 +126,16 @@ const Excharge = styled.div`
   }
 `;
 const Profile = styled.div`
+  width: 340px;
+  margin-top: 25px;
+  .text {
+    margin-bottom: 12px;
+    color: var(--gray_02, #b5b5b5);
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
   hr {
     width: 339px;
     height: 1px;
@@ -110,9 +143,27 @@ const Profile = styled.div`
   img {
     width: 24px;
     height: 24px;
+    margin-right: 16px;
+    margin-left: auto;
+  }
+  .profile-item {
+    display: flex;
+    width: 340px;
+    height: 40px;
+    align-items: center;
   }
 `;
 const Account = styled.div`
+  width: 340px;
+  margin-top: 32px;
+  .text {
+    margin-bottom: 12px;
+    color: var(--gray_02, #b5b5b5);
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
   hr {
     width: 339px;
     height: 1px;
@@ -120,5 +171,13 @@ const Account = styled.div`
   img {
     width: 24px;
     height: 24px;
+    margin-right: 16px;
+    margin-left: auto;
+  }
+  .account-item {
+    display: flex;
+    width: 340px;
+    height: 40px;
+    align-items: center;
   }
 `;
