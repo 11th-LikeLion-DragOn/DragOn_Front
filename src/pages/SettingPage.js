cdca6325d77308b7f8bb5282db87565b9cbc5aea
binys21 <binys1121@ewhain.net>
2023-11-17 15:02:37 +0900

    feat: 설정페이지 수정 중

--- src/pages/SettingPage.js
+++ src/pages/SettingPage.js
@@ -11,48 +11,55 @@ const SettingPage = () => {
   return (
     <>
       <TopBar titleText="설정" />
-      <Info>
-        <img src={profile1} />
-        <div>농담곰</div>
-        <div className="nickname">damgom333</div>
-      </Info>
+      <Wrapper>
+        <Info>
+          <img src={profile1} />
+          <div className="name">농담곰</div>
+          <div className="nickname">damgom333</div>
+        </Info>
 
-      <Excharge>
-        보유한 여의주 1개
-        <img src={marblePurple} />
-        충전하기
-      </Excharge>
-      <Profile>
-        <span>프로필</span>
-        <div>
-          닉네임 변경 <img src={clickRight} />
-        </div>
-        <hr></hr>
-        <div>
-          챌린지 성향 테스트 <img src={clickRight} />
-        </div>
-        <hr></hr>
-      </Profile>
-      <Account>
-        <span>계정</span>
-        <div>
-          비밀번호 변경 <img src={clickRight} />
-        </div>
-        <hr></hr>
-        <div>
-          로그아웃 <img src={clickRight} />
-        </div>
-        <hr></hr>
-        <div>
-          계정 탈퇴 <img src={clickRight} />
-        </div>
-        <hr></hr>
-      </Account>
+        <Excharge>
+          보유한 여의주 1개
+          <div className="chargebtn">
+            <img src={marblePurple} />
+            충전하기
+          </div>
+        </Excharge>
+        <Profile>
+          <span>프로필</span>
+          <div>
+            닉네임 변경 <img src={clickRight} />
+          </div>
+          <hr></hr>
+          <div>
+            챌린지 성향 테스트 <img src={clickRight} />
+          </div>
+          <hr></hr>
+        </Profile>
+        <Account>
+          <span>계정</span>
+          <div>
+            비밀번호 변경 <img src={clickRight} />
+          </div>
+          <hr></hr>
+          <div>
+            로그아웃 <img src={clickRight} />
+          </div>
+          <hr></hr>
+          <div>
+            계정 탈퇴 <img src={clickRight} />
+          </div>
+          <hr></hr>
+        </Account>
+      </Wrapper>
     </>
   );
 };
 
 export default SettingPage;
+const Wrapper = styled.div`
+  padding: 26px 25px 0px 28px;
+`;
 const Info = styled.div`
   display: flex;
   img {
@@ -60,17 +67,58 @@ const Info = styled.div`
     height: 57px;
     flex-shrink: 0;
   }
+  .name {
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+  .nickname {
+    color: var(--gray_02, #b5b5b5);
+    font-feature-settings: "clig" off, "liga" off;
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
 `;
-const Excharge = styled.div``;
+const Excharge = styled.div`
+  display: flex;
+  width: 340px;
+  height: 47px;
+  border-radius: 6px;
+  background: var(--purple_02, #f5f1ff);
+  img {
+    margin-left: 144px;
+  }
+  .chargebtn {
+    color: var(--purple_01, #8438ff);
+    font-feature-settings: "clig" off, "liga" off;
+    font-family: Pretendard;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+`;
 const Profile = styled.div`
   hr {
     width: 339px;
     height: 1px;
+  }
+  img {
+    width: 24px;
+    height: 24px;
   }
 `;
 const Account = styled.div`
   hr {
     width: 339px;
     height: 1px;
+  }
+  img {
+    width: 24px;
+    height: 24px;
   }
 `;
