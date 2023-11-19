a2b4c6f14f9aa7ab1f52445d6087135cf0a28e80
y0ung22 <amy6484@ewhain.net>
2023-11-20 00:06:06 +0900

    feat: 홈화면 모은 여의주&달성률 박스 컴포넌트 제작

---
+++ src/pages/MainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+import StatusBox from "../components/MainPage/StatusBox";
+
+const MainPage = () => {
+  return (
+    <>
+      <StatusBox />
+    </>
+  );
+};
+
+export default MainPage;
