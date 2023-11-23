5ba64ccbd61cb353e2307442152d94a4b59d4708
binys21 <binys1121@ewhain.net>
2023-11-23 19:10:36 +0900

    feat: 진행 중인 챌린지 없을 때 화면 추가

---
+++ src/components/ChallengeListPage/NoChallenge.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const NoChallenge = () => {
+  return (
+    <>
+      <Wrapper>
+        <div className="text">
+          진행 중인 챌린지가 없습니다. 새로운 챌린지를 생성해주세요.
+        </div>
+        <Btn>챌린지 생성하기</Btn>
+      </Wrapper>
+    </>
+  );
+};
+export default NoChallenge;
+const Wrapper = styled.div`
+  width: 393px;
+  height: 228px;
+  .text {
+    color: var(--gray_02, #b5b5b5);
+    text-align: center;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+`;
+const Btn = styled.div`
+  display: inline-flex;
+  padding: 15px 12px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 6px;
+  background: var(--purple_01, #8438ff);
+  color: var(--white, #fff);
+  text-align: center;
+  font-feature-settings: "clig" off, "liga" off;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
