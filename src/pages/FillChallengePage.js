cc408e610ff1aee501df1056ea32824af7fef6b6
y0ung22 <amy6484@ewhain.net>
2023-11-29 21:13:55 +0900

    feat: 챌린지 메꾸기 페이지 추가

--- src/pages/FillChallengePage.js
+++ src/pages/FillChallengePage.js
@@ -1,8 +1,41 @@
 import React from "react";
 import { styled } from "styled-components";
 
+import TopBar from "../components/common/TopBar";
+import Challenge from "../components/MainPage/Challenge";
+
 const FillChallengePage = () => {
-  return <></>;
+  return (
+    <Wrapper>
+      <TopBar titleText={"챌린지 메꾸기"} />
+      <ChallengeList>
+        <Challenge />
+      </ChallengeList>
+    </Wrapper>
+  );
 };
 
 export default FillChallengePage;
+
+const Wrapper = styled.div`
+  margin: auto auto;
+  width: 393px;
+  height: 852px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ChallengeList = styled.div`
+  margin-top: 34px;
+  display: flex;
+  width: 315px;
+  padding: 18px 19px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+  border-radius: 14px;
+  border: 1px solid rgba(199, 198, 198, 0.2);
+  background: var(--white);
+`;
