9b8b48a898f245fd911f23e87cc834f6941c8fa2
binys21 <binys1121@ewhain.net>
2023-12-04 13:42:38 +0900

    feat: 설문조사 페이지 선택 옵션 기능 추가

--- src/pages/TestingPage.js
+++ src/pages/TestingPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled } from "styled-components";
 //components
 import TopBar from "../components/common/TopBar";
@@ -7,6 +7,22 @@ import grayCheck from "../assets/icons/test-check-gray.png";
 import pupleCheck from "../assets/icons/test-check-purple.png";
 
 const TestingPage = () => {
+  // 각 질문에 대해 사용자 선택(yes/no)을 저장
+  const [selectedOptions, setSelectedOptions] = useState({
+    q1: null,
+    q2: null,
+    q3: null,
+    q4: null,
+    q5: null,
+  });
+
+  const handleOptionClick = (questionNumber, option) => {
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      [questionNumber]: prevOptions[questionNumber] === option ? null : option,
+    }));
+  };
+
   return (
     <>
       <TopBar titleText="챌린지 성향 테스트" />
@@ -16,26 +32,54 @@ const TestingPage = () => {
           <div className="text">
             작심삼일, 나는 계획을 끝까지 지켜본 적이 잘 없다.
           </div>
-          <div className="yesBox">
-            <img src={grayCheck} />
+          <div
+            className={`yesBox ${
+              selectedOptions.q1 === "yes" ? "selected" : ""
+            }`}
+            onClick={() => handleOptionClick("q1", "yes")}
+          >
+            <img
+              src={selectedOptions.q1 === "yes" ? pupleCheck : grayCheck}
+              alt="Check Icon"
+            />
             Yes
           </div>
           <hr></hr>
-          <div className="noBox">
-            <img src={grayCheck} />
+          <div
+            className={`noBox ${selectedOptions.q1 === "no" ? "selected" : ""}`}
+            onClick={() => handleOptionClick("q1", "no")}
+          >
+            <img
+              src={selectedOptions.q1 === "no" ? pupleCheck : grayCheck}
+              alt="Check Icon"
+            />
             No
           </div>
         </Q1>
         <Q2>
           <div className="number">Q2.</div>
           <div className="text">계획은 최대한 구체적으로, 자세하게 세운다</div>
-          <div className="yesBox">
-            <img src={grayCheck} />
+          <div
+            className={`yesBox ${
+              selectedOptions.q2 === "yes" ? "selected" : ""
+            }`}
+            onClick={() => handleOptionClick("q2", "yes")}
+          >
+            <img
+              src={selectedOptions.q2 === "yes" ? pupleCheck : grayCheck}
+              alt="Check Icon"
+            />
             Yes
           </div>
           <hr></hr>
-          <div className="noBox">
-            <img src={grayCheck} />
+          <div
+            className={`noBox ${selectedOptions.q2 === "no" ? "selected" : ""}`}
+            onClick={() => handleOptionClick("q2", "no")}
+          >
+            <img
+              src={selectedOptions.q2 === "no" ? pupleCheck : grayCheck}
+              alt="Check Icon"
+            />
             No
           </div>
         </Q2>
@@ -43,39 +87,81 @@ const TestingPage = () => {
           <div className="number">Q3.</div>
 
           <div className="text">장기 목표보다 단기 목표에 강하다</div>
-          <div className="yesBox">
-            <img src={grayCheck} />
+          <div
+            className={`yesBox ${
+              selectedOptions.q3 === "yes" ? "selected" : ""
+            }`}
+            onClick={() => handleOptionClick("q3", "yes")}
+          >
+            <img
+              src={selectedOptions.q3 === "yes" ? pupleCheck : grayCheck}
+              alt="Check Icon"
+            />
             Yes
           </div>
           <hr></hr>
-          <div className="noBox">
-            <img src={grayCheck} />
+          <div
+            className={`noBox ${selectedOptions.q3 === "no" ? "selected" : ""}`}
+            onClick={() => handleOptionClick("q3", "no")}
+          >
+            <img
+              src={selectedOptions.q3 === "no" ? pupleCheck : grayCheck}
+              alt="Check Icon"
+            />
             No
           </div>
         </Q3>
         <Q4>
           <div className="number">Q4.</div>
           <div className="text">챌린지의 시작을 지인들에게 널리 알린다</div>
-          <div className="yesBox">
-            <img src={grayCheck} />
+          <div
+            className={`yesBox ${
+              selectedOptions.q4 === "yes" ? "selected" : ""
+            }`}
+            onClick={() => handleOptionClick("q4", "yes")}
+          >
+            <img
+              src={selectedOptions.q4 === "yes" ? pupleCheck : grayCheck}
+              alt="Check Icon"
+            />
             Yes
           </div>
           <hr></hr>
-          <div className="noBox">
-            <img src={grayCheck} />
+          <div
+            className={`noBox ${selectedOptions.q4 === "no" ? "selected" : ""}`}
+            onClick={() => handleOptionClick("q4", "no")}
+          >
+            <img
+              src={selectedOptions.q4 === "no" ? pupleCheck : grayCheck}
+              alt="Check Icon"
+            />
             No
           </div>
         </Q4>
         <Q5>
           <div className="number">Q5.</div>
           <div className="text">목표 달성보다는 과정에 의의를 둔다</div>
-          <div className="yesBox">
-            <img src={grayCheck} />
+          <div
+            className={`yesBox ${
+              selectedOptions.q5 === "yes" ? "selected" : ""
+            }`}
+            onClick={() => handleOptionClick("q5", "yes")}
+          >
+            <img
+              src={selectedOptions.q5 === "yes" ? pupleCheck : grayCheck}
+              alt="Check Icon"
+            />
             Yes
           </div>
           <hr></hr>
-          <div className="noBox">
-            <img src={grayCheck} />
+          <div
+            className={`noBox ${selectedOptions.q5 === "no" ? "selected" : ""}`}
+            onClick={() => handleOptionClick("q5", "no")}
+          >
+            <img
+              src={selectedOptions.q5 === "no" ? pupleCheck : grayCheck}
+              alt="Check Icon"
+            />
             No
           </div>
         </Q5>
@@ -113,6 +199,9 @@ const Wrapper = styled.div`
     gap: 10px;
     border-radius: 11px 11px 0px 0px;
     background: var(--background_02, #fafafa);
+    &.selected {
+      background: var(--purple_02, #f5f1ff);
+    }
   }
   .noBox {
     width: 337px;
@@ -121,7 +210,10 @@ const Wrapper = styled.div`
     gap: 10px;
     border-radius: 0px 0px 11px 11px;
     background: var(--background_02, #fafafa);
-    /* background: var(--purple_02, #f5f1ff); 클릭했을 때 색상 */
+    &.selected {
+      background: var(--purple_02, #f5f1ff);
+    }
+
     margin-bottom: 34px;
   }
   hr {
