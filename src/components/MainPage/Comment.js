669f01a73845c7de1982eb6cb083cd3a41944a9a
y0ung22 <amy6484@ewhain.net>
2023-12-18 21:40:33 +0900

    feat: 댓글 드롭박스 추가

--- src/components/MainPage/Comment.js
+++ src/components/MainPage/Comment.js
@@ -4,8 +4,10 @@ import { styled } from "styled-components";
 import profile from "../../assets/icons/profile5.png";
 import more from "../../assets/icons/more.png";
 import RecommentBox from "./RecommentBox";
+import trash from "../../assets/icons/trash.png";
 
 const Comment = () => {
+  const [openDropBox, setOpenDropBox] = useState(false);
   const [content, setContent] = useState(
     "님 어제 저랑 술마셨잖아요ㅋㅋ 이게 머죠?????"
   );
@@ -20,7 +22,14 @@ const Comment = () => {
           <span id="time">2023.11.16 13:14</span>
         </ProfileText>
       </Profile>
-      <img id="more" src={more} />
+      <img id="more" src={more} onClick={() => setOpenDropBox(!openDropBox)} />
+      {openDropBox && (
+        <DropBox>
+          <span id="edit">수정</span>
+          <div id="line"></div>
+          <span id="del">삭제</span>
+        </DropBox>
+      )}
       <Content>{content}</Content>
       <Recomment onClick={() => setOpen(!open)}>답글</Recomment>
       {open && <RecommentBox />}
@@ -82,6 +91,38 @@ const ProfileText = styled.div`
     font-style: normal;
     font-weight: 400;
     line-height: normal;
+  }
+`;
+
+const DropBox = styled.div`
+  position: absolute;
+  top: 34px;
+  right: 12px;
+  z-index: 50;
+  display: flex;
+  width: 49px;
+  padding: 4px 5px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 3px;
+  border-radius: 4px;
+  border: 0.7px solid var(--gray3);
+  background: rgba(199, 198, 198, 0.2);
+
+  color: var(--gray2);
+  text-align: center;
+  font-feature-settings: "clig" off, "liga" off;
+  font-family: Pretendard;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+
+  #line {
+    width: 42px;
+    height: 0.7px;
+    background-color: var(--gray2);
   }
 `;
 
