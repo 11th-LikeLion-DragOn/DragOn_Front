389d212008528afb0882cb158b82957c1d550eca
y0ung22 <amy6484@ewhain.net>
2024-01-01 13:55:46 +0900

    feat: 달력 코드 구조 및 css 조건 추가

--- src/components/MainPage/Challenge.js
+++ src/components/MainPage/Challenge.js
@@ -3,9 +3,24 @@ import * as dateFns from "date-fns";
 import { styled } from "styled-components";
 
 const Challenge = ({ selectedDate }) => {
-  const [goal1, setGoal1] = useState(false);
+  const [goal1, setGoal1] = useState(true);
   const [goal2, setGoal2] = useState(false);
   const [goal3, setGoal3] = useState(false);
+
+  const handleGoal1 = () => {
+    setGoal1(!goal1);
+    console.log(goal1);
+  };
+
+  const handleGoal2 = () => {
+    setGoal2(!goal2);
+    console.log(goal2);
+  };
+
+  const handleGoal3 = () => {
+    setGoal3(!goal3);
+    console.log(goal3);
+  };
 
   return (
     <Wrapper>
@@ -15,15 +30,15 @@ const Challenge = ({ selectedDate }) => {
       </Date>
       <ChallengeList>
         <Goal1>
-          <div id="circle" goal1={goal1}></div>
+          <div id="circle" goal1={goal1} onClick={handleGoal1}></div>
           <span>영어 공부</span>
         </Goal1>
         <Goal2>
-          <div id="circle" goal2={goal2}></div>
+          <div id="circle" goal2={goal2} onClick={handleGoal2}></div>
           <span>스페인어 화상수업</span>
         </Goal2>
         <Goal3>
-          <div id="circle" goal3={goal3}></div>
+          <div id="circle" goal3={goal3} onClick={handleGoal3}></div>
           <span>매일 감사일기 작성하기</span>
         </Goal3>
       </ChallengeList>
@@ -74,7 +89,9 @@ const Goal1 = styled.div`
     height: 15px;
     flex-shrink: 0;
     border-radius: 50%;
-    background-color: var(--red);
+    background-color: ${({ goal1 }) => (goal1 ? "var(--red)" : "var(--white)")};
+    border: ${({ goal1 }) => (goal1 ? "none" : "1px solid var(--gray2)")};
+    cursor: pointer;
   }
 `;
 
@@ -88,7 +105,10 @@ const Goal2 = styled.div`
     height: 15px;
     flex-shrink: 0;
     border-radius: 50%;
-    background-color: var(--green);
+    background-color: ${({ goal2 }) =>
+      goal2 ? "var(--green)" : "var(--white)"};
+    border: ${({ goal2 }) => (goal2 ? "none" : "1px solid var(--gray2)")};
+    cursor: pointer;
   }
 `;
 
@@ -102,6 +122,9 @@ const Goal3 = styled.div`
     height: 15px;
     flex-shrink: 0;
     border-radius: 50%;
-    background-color: var(--blue);
+    background-color: ${({ goal3 }) =>
+      goal3 ? "var(--blue)" : "var(--white)"};
+    border: ${({ goal3 }) => (goal3 ? "none" : "1px solid var(--gray2)")};
+    cursor: pointer;
   }
 `;
--- src/components/MainPage/DayStatus.js
+++ src/components/MainPage/DayStatus.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { styled } from "styled-components";
 
-const DayStatus = (isToday) => {
+const DayStatus = () => {
   return (
     <Wrapper>
       <Goal1></Goal1>
