f6909b3c766f6754ecb4ccd2129be3c4812d66f0
y0ung22 <amy6484@ewhain.net>
2024-01-08 18:13:21 +0900

    feat: 챌린지 달성 현황 함수

--- src/api/challenge.js
+++ src/api/challenge.js
@@ -145,3 +145,13 @@ export const WriteRecomment = async (commentId, text) => {
     console.error("대댓글 작성 실패", error);
   }
 };
+
+//챌린지 달성 현황 조회
+export const GetChallengeStatus = async () => {
+  try {
+    const response = await http.get("/main/achieverate/");
+    return response;
+  } catch (error) {
+    console.error("챌린지 달성 현황 조회 실패", error);
+  }
+};
